de20ed0b15dafc694dfa9b055db57a188aff019d
pace11 <ryanjoker87@gmail.com>
2023-10-02 01:56:01 +0700

    feat: add swagger docs api

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,14 +5,14 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'Documentations API',
             ],
 
             'routes' => [
                 /*
                  * Route for accessing api documentation interface
                 */
-                'api' => 'api/documentation',
+                'api' => 'api/docs',
             ],
             'paths' => [
                 /*
